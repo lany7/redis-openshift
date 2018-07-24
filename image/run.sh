96bbcb4d53cbc85e33c6a0d3a8bf41d03748901d
lany7 <lars.nyman@gmail.com>
2018-07-24 12:06:48 +0200

    Update run.sh

--- image/run.sh
+++ image/run.sh
@@ -41,6 +41,7 @@ function launchsentinel() {
 
   sentinel_conf=/redis-sentinel/sentinel.conf
 
+  echo "protected-mode no" > ${sentinel_conf}
   echo "sentinel monitor mymaster ${master} 6379 2" > ${sentinel_conf}
   echo "sentinel down-after-milliseconds mymaster 60000" >> ${sentinel_conf}
   echo "sentinel failover-timeout mymaster 180000" >> ${sentinel_conf}
